cd055a04f901ebdd701ea5c6d1426b5c564f9fa1
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-06-26 00:28:14 +0700

    add odin-authenication-basics

---
+++ odin-authentication-basics/app.js
@@ -0,0 +1,115 @@
+/////// app.js
+
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+const mongoDb =
+  'mongodb+srv://admin:idontreallycare@cluster0.wimue0i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+const app = express();
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      // if (user.password !== password) {
+      //   return done(null, false, { message: 'Incorrect password' });
+      // }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: 'Incorrect password' });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { user: req.user });
+});
+
+app.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  })
+);
+app.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+app.post('/sign-up', async (req, res, next) => {
+  // try {
+  //   const user = new User({
+  //     username: req.body.username,
+  //     password: req.body.password,
+  //   });
+  //   const result = await user.save();
+  //   res.redirect('/');
+  // } catch (err) {
+  //   return next(err);
+  // }
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    }
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
---
+++ odin-authentication-basics/readme.md
@@ -0,0 +1,7 @@
+# PassportJS
+
+npm install mongoose express-async-handler express-validator
+npm install bcryptjs
+
+Authentication
+Local method
